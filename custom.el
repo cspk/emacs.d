54fc178b08e007e47818c469fea04cd09e4b77bb
Pavel Serebryakov <serebryakovp@progressufa.ru>
2019-07-04 14:20:59 +0500

    packages: add xterm-keybinder
    This allows using more Emacs chords while in console (urxvt).

--- custom.el
+++ custom.el
@@ -13,7 +13,7 @@
 	("e0a4eb9488dacb5a981fc499cd70a028dd0a954e8a91c61e87b959fd2daba027" "723e09e06525b35c171cd0d20693c460d76431971278d75f5f97d0d70a2cfd36" "4d1d88ca272116c208dd2e7058512ca77d0e97a2205a0ee9159e46447f2c7c36" "b0967aab4b05860a35e905d6268e88726f3360755349cbd8a20a31cda9c8f64a" "b5baa157c544935154ebce57d34d5f06025165b00e1194b99167dd2d8c7100e9" "2b7370e336c6770fb2dec1742aff3244e36a2073121ea354dc090bc6b80152ac" "50b48c8ab36a88709d5f808c250a07cf6b2cb46fa3f434918491c9132e51a2e2" "e32b024474fb4f5a5cf883e1701c0ef517d3bad5948ec510571ad931167894bb" "e66cce8cf23fcf78616de6daab96a04af8ee0495330576ca411fce11bef4c9cc" "d7a70e8659d264f0ce15b87ea830e65617f3a76021d55aff34b58c43f7076695" "1b5606a77b9f44c2ae0e3b5f355ead6331825742d52e2eec9f0fb55103d196a4" "3337d84eb3081b069599f9dc8bcfa69f1923f22c9ca27a2e9a625143456daf5c" "28ff365a6c60085b01a79640f71ed43e8f23852dc673ca88d573f0c84503952e" "c8e7ae72efee8504ec16bdacae8ff29e9334f27080620fdd1cacfb627b4aac4b" default)))
  '(package-selected-packages
    (quote
-	(clang-format whitespace-cleanup-mode org-pomodoro php-mode anzu multiple-cursors easy-kill xclip fill-column-indicator terminal-focus-reporting git-commit dtrt-indent smart-tabs-mode total-lines helm-projectile doom-modeline ctags-update helm-cscope evil helm projectile diff-hl undo-tree))))
+	(xterm-keybinder clang-format whitespace-cleanup-mode org-pomodoro php-mode anzu multiple-cursors easy-kill xclip fill-column-indicator terminal-focus-reporting git-commit dtrt-indent smart-tabs-mode total-lines helm-projectile doom-modeline ctags-update helm-cscope evil helm projectile diff-hl undo-tree))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
